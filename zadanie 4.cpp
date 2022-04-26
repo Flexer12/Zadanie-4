29e58367adb6d4b9e2f0e19863399cbce286b95f
Flexer12 <52821702+Flexer12@users.noreply.github.com>
2022-04-26 17:27:35 +0300

    Add files via upload

---
+++ zadanie 4.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double a, b, z, t;
+    cout << "Enter a : "<<endl;
+    cin >> a;
+    cout << "Enter b : " << endl;
+    cin >> b;
+    if (a >= b) {
+        z = 1 - cos(a) * 2 * sin(b);
+    }
+    else {
+        z = sqrt(abs(pow(a, 2) - pow(b, 2)));
+    }
+    if (z < 0 ) {
+        cout << "Error";
+        return 0;
+    }
+    if (z > b) {
+        t = 1 / cos(z * a);
+    }
+    if (z == b) {
+        t = 1-log(z)+cos(pow(a,2)*b);
+    }
+    else {
+        t = pow(z + pow(a, 2) * b,1/3);
+    }
+    
+    cout << "z=" << z << endl << "t=" << t;
+
+
+}
+
